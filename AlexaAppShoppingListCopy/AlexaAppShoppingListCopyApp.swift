bc41243a9d346415bef3dc4a99830cf50ead7f40
Andy Kayley <bitbucket@kayley.name>
2022-04-10 21:28:00 +0100

    Initial Commit

---
+++ AlexaAppShoppingListCopy/AlexaAppShoppingListCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlexaAppShoppingListCopyApp.swift
+//  AlexaAppShoppingListCopy
+//
+//  Created by Andy Kayley on 10/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct AlexaAppShoppingListCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
