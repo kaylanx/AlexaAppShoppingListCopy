40647f1cb9ab91f28387ec2b26a60952c7ddeebb
Andy Kayley <bitbucket@kayley.name>
2022-04-11 21:16:54 +0100

    Initial Commit

--- AlexaAppShoppingListCopy/ContentView.swift
+++ AlexaAppShoppingListCopy/ContentView.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+let alexaBlueColor = Color(red:0.05, green: 0.39, blue: 0.66)
+
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Button {
+
+                } label: {
+                    Text("Add Item".uppercased())
+                        .bold()
+                        .padding(2)
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                }
+                .buttonStyle(.bordered)
+                .background(alexaBlueColor)
+                .cornerRadius(5)
+                .padding(10)
+
+                Spacer()
+            }
+            .navigationTitle("Shopping".uppercased())
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                HStack {
+                    Button("Share") {
+
+                    }
+                    .foregroundColor(.primary)
+                    Button {
+
+                    } label: {
+                        Image(systemName: "ellipsis")
+                            .rotationEffect(.degrees(-90))
+                    }
+                    .foregroundColor(.primary)
+                }
+            }
+        }
     }
 }
 
