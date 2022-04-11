a7f75aecd97ad333a7e6a993bf1fcb5f2284c72e
Andy Kayley <bitbucket@kayley.name>
2022-04-11 23:02:28 +0100

    Add the list in

--- AlexaAppShoppingListCopy/ContentView.swift
+++ AlexaAppShoppingListCopy/ContentView.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 let alexaBlueColor = Color(red:0.05, green: 0.39, blue: 0.66)
 
+struct Item: Identifiable {
+    var id: String {
+        name
+    }
+
+    let name: String
+    var completed: Bool
+}
+
+struct CheckBoxView: View {
+    @Binding var checked: Bool
+
+    var body: some View {
+        Image(systemName: checked ? "checkmark.square.fill" : "square")
+            .foregroundColor(checked ? alexaBlueColor : Color.secondary)
+            .onTapGesture {
+                self.checked.toggle()
+            }
+    }
+}
+
+struct ListItemView: View {
+
+    @State var item: Item
+
+    var body: some View {
+        HStack {
+            CheckBoxView(checked: $item.completed)
+            Text(item.name)
+        }
+    }
+}
+
 struct ContentView: View {
+
+    private let items = [
+        Item(name: "Oxo cubes", completed: false),
+        Item(name: "Flavoured water", completed: false),
+        Item(name: "Baby wipes", completed: false),
+        Item(name: "Drainer", completed: false),
+        Item(name: "Bread knife 🔪", completed: false),
+        Item(name: "Piping bag", completed: false),
+
+        Item(name: "Potatoes", completed: true),
+        Item(name: "Bread", completed: true),
+        Item(name: "Rinse aid", completed: true),
+    ]
 
     var body: some View {
         NavigationView {
@@ -27,6 +73,29 @@ struct ContentView: View {
                 .background(alexaBlueColor)
                 .cornerRadius(5)
                 .padding(10)
+
+                List {
+
+                    ForEach(items) { item in
+                        if (item.completed == false) {
+                            ListItemView(item: item)
+                        }
+                    }
+
+                    Section {
+                        ForEach(items) { item in
+                            if (item.completed == true) {
+                                ListItemView(item: item)
+                            }
+                        }
+                    } header: {
+                        Text("Completed")
+                            .bold()
+                            .foregroundColor(.black)
+                    }
+                }
+                .listStyle(.grouped)
+
 
                 Spacer()
             }
